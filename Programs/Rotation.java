71c59b17846e60fe15709fb660690f00ac6538e5
KahanPrabhu <kprabhu@ncsu.edu>
2016-03-02 15:22:05 -0500

    Rotation.java

---
+++ Programs/Rotation.java
@@ -0,0 +1,51 @@
+import java.io.*;
+public class Rotation
+{
+    public static void main()throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter string 1");
+        String s1= in.readLine();
+        System.out.println("Enter string 2");
+        String s2= in.readLine();
+        
+        System.out.println("S2 is a substring of S1 even after rotation? Answer: "+isSubstring(s1, s2));
+    }
+    
+    
+    static boolean isSubstring(String s1, String s2)
+    {
+        
+        String temp = s2+s2;
+        System.out.println("Value of temp ="+temp);
+        int flag=0;
+        boolean choice=false;
+        if(temp.contains(s1))
+        {
+            for(int i=0;i<temp.length()-s1.length();i++)
+            {
+                System.out.println(temp.substring(i,i+s1.length()));
+                if(String.valueOf(temp.substring(i,i+s1.length())).equals(s1))
+                {
+                    System.out.println("Inside If case");
+                    flag=i; break;
+                }
+                
+            }
+            
+            String str=String.valueOf(temp.substring(flag+s1.length(),temp.length()))+String.valueOf(temp.substring(0,flag));
+            
+            if(str.equals(s1))
+            {
+                choice=true;
+            }
+            
+        }
+        
+        
+        return choice;
+        
+    }
+    
+    
+}
