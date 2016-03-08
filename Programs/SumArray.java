79afc062af3bb439f50c3e183279b542ecae81a9
KahanPrabhu <kprabhu@ncsu.edu>
2016-03-07 20:19:22 -0500

    SumArray.java

---
+++ Programs/SumArray.java
@@ -0,0 +1,76 @@
+import java.io.*;
+public class SumArray
+{
+    static int arr[];
+    public static void main()throws IOException
+    {
+        
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the array length");
+        int n=Integer.parseInt(in.readLine());
+        arr=new int[n];
+        System.out.println("Enter the elements of the array");
+        
+        for(int i=0; i<n; i++)
+        {
+            arr[i]= Integer.parseInt(in.readLine());
+            
+        }
+       
+        System.out.println("Enter the value of the sum");
+        int sum = Integer.parseInt(in.readLine());
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i;j<arr.length;j++)
+            {
+                if(arr[j]>arr[i])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+                
+            }
+            }
+        
+ 
+        int anchor = (int)(sum/2);
+        int u=findClosest(anchor);
+      
+            
+        
+        System.out.println("The pairs that sum up to "+sum+" are : ");
+        
+        for(int i=u; i<arr.length;i++)
+        {
+            for(int j=u-1;j>=0;j--)
+            {
+                if(arr[i]+arr[j]== sum)
+                {
+                    System.out.println(arr[i]+", "+arr[j]);
+                }
+            }
+        }
+   
+    }
+    
+    static int findClosest(int value)
+    {
+        int index=0;
+       int base= Math.abs(arr[0]-value); 
+        for(int i=1;i<arr.length;i++)
+        {
+            if(Math.abs(arr[i]- value)<base)
+            {
+                base =Math.abs(arr[i]-value); index= i;
+            }
+        }
+        
+        return index;
+        
+    }
+    
+    
+    
+}
